ad0f583b52d9456670fd364089d0323823b4f75f
azhararrozak <azhardrozak2001@upi.edu>
2023-11-30 13:38:47 +0700

    Perbaiki Dashboard dan notebook

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,44 +3,43 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Load data
+# Load dataset
 bike_day_df = pd.read_csv("https://raw.githubusercontent.com/azhararrozak/Submission-Proyek-Analisis-Data/main/dataset/day.csv")
 
-# Set page title
-st.title("Bike Sharing Data Dashboard")
+# Judul Dashboard
+st.title("Dashboard: Analisis Data Peminjaman Sepeda")
 
-# Display the first few rows of the dataset
+# Menampilkan Dataset
 st.subheader("Dataset Preview")
 st.write(bike_day_df.head())
 
-# Visualization: Bar plot for bike rentals based on weather situation
+# Visualisasi Peminjaman sepeda berdasarkan cuaca
 st.subheader("Bike Rentals Based on Weather Condition")
 fig, ax = plt.subplots(figsize=(8, 6))
 sns.barplot(x="weathersit", y="cnt", data=bike_day_df, palette="viridis", ax=ax)
-plt.xlabel("Weather Situation")
-plt.ylabel("Total Bike Rentals")
-plt.title("Total Bike Rentals Based on Weather Condition")
+plt.xlabel("Kondisi Cuaca")
+plt.ylabel("Jumlah Pengguna Sepeda")
+plt.title("Jumlah Pengguna Sepeda Berdasarkan Kondisi Cuaca")
 st.pyplot(fig)
 
-# Additional information
+# Tambahan Informasi
 st.markdown("### Additional Information:")
 st.markdown(
-    "1. **Weather Situation Codes:**\n"
-    "   - 1: Clear, Few clouds, Partly cloudy, Partly cloudy\n"
-    "   - 2: Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist\n"
-    "   - 3: Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds\n"
-    "   - 4: Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog"
+    "**Weather Situation Codes:**\n"
+    "   - 1: Clear\n"
+    "   - 2: Cloudy\n"
+    "   - 3: Rain\n"
 )
 
-# vizualization: line chart for bike rentals in 2011 and 2012
+# Visualisasi Peminjaman sepeda pada tahun 2011 dan 2012
 dtedayPD = pd.to_datetime(bike_day_df['dteday'])
 
-st.subheader("Bike Rentals in 2011 and 2012")
+st.subheader("Peminjaman Sepeda pada Tahun 2011 dan 2012")
 fig, ax = plt.subplots(figsize=(8, 6))
 sns.lineplot(x=dtedayPD, y="cnt", data=bike_day_df, hue="yr", palette="viridis", ax=ax)
-plt.xlabel("Date")
-plt.ylabel("Total Bike Rentals")
-plt.title("Total Bike Rentals in 2011 and 2012")
+plt.xlabel("Tanggal")
+plt.ylabel("Total Peminjaman Sepeda")
+plt.title("Tren Peminjaman Sepeda pada Tahun 2011 dan 2012")
 plt.xticks(rotation=45)
 st.pyplot(fig)
 
